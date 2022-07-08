71af4ad3c724f15d09436de4d66b38c25d9c436c
AC1556 <allen@urbanlife.me>
2022-07-08 18:39:57 +0900

    Tailwind global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "sol-tan-1": "#fdf6e3",
+      "sol-tan-2": "#eee8d5",
+      "sol-blue-1": "#268bd2",
+      "sol-cyan-1": "#2aa198",
+      "sol-yellow-1": "#b58900",
+      "sol-grey-4": "#586e75",
+      "sol-grey-3": "#657b83",
+      "sol-grey-2": "#839496",
+      "sol-grey-1": "#93a1a1",
+      "sol-black-1": "#073642",
+      "sol-black-2": "#002b36",
+      "white-1": "#ffffff",
+      "sol-red-1": "#c9211e",
+      "sol-magenta-1": "#d33682",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase }) {
+      addBase({});
+    },
+  ],
+  darkMode: "class",
 };
