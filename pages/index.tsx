e3beff03d1687a241d4efffc7e170e096c822de9
AC1556 <allenclark@u.boisestate.edu>
2022-07-09 23:49:44 +0900

    Removing global styles from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,22 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import LocationList from '../components/location/LocationList';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Open Coffee</title>
         <meta name="description" content="Open platform to discover new cafes!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Open Coffee</h1>
-        <div className="w-full p-8 max-w-5xl ml-auto mr-auto">
+        <main className="w-full p-8 max-w-5xl ml-auto mr-auto">
+        <h1 className="whitespace-nowrap">Open Coffee</h1>
           <LocationList />
-        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+      <footer>
       </footer>
     </div>
   );
